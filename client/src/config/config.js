e3258699a1819d3aa431854ad9a3a5dea439a163
Chidera <kharunaking@gmail.com>
2023-07-23 17:59:32 -0400

    Threejs 3D project

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://threejs-3d-backend.onrender.com",
+    backendUrl: "https://threejs-3d-backend.onrender.com/api/v1/dalle",
   },
 };
 
