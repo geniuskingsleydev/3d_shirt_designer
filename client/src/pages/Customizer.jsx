ba04ca2f99fdb54a0c8adbcc8a6c4b4e5906e15c
Chidera <kharunaking@gmail.com>
2023-07-23 17:51:30 -0400

    Threejs 3D project

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -75,15 +75,18 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
+      const response = await fetch(
+        "https://threejs-3d-backend.onrender.com/api/v1/dalle",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
+        }
+      );
 
       const data = await response.json();
 
